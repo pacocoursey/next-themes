64196a6d789ac6e577075a7824af02ae5c0114f5
Tobias Maier <trm217@protonmail.com>
2022-02-20 18:21:57 +0100

    update comments on the resolved-theme attribute

--- src/types.ts
+++ src/types.ts
@@ -9,7 +9,11 @@ export interface UseThemeProps {
   setTheme: (theme: string) => void
   /** Active theme name */
   theme?: string
-  /** If `enableSystem` is true and the active theme is "system", this returns whether the system preference resolved to "dark" or "light". Otherwise, identical to `theme` */
+  /**
+   * If `enableSystem` is true and the active theme is "system", this returns whether the system preference resolved to "dark" or "light".
+   * In case a theme is forced, the forced theme value is returned.
+   * Otherwise, identical to `theme`
+   */
   resolvedTheme?: string
   /** If enableSystem is true, returns the System theme preference ("dark" or "light"), regardless what the active theme is */
   systemTheme?: 'dark' | 'light'

43f1e631dbaa0094af39d4effa768b631d04cb63
Tobias Maier <trm217@protonmail.com>
2022-05-10 12:53:27 +0200

    add GitHub Action to execute e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,10 @@ const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   reporter: process.env.CI ? 'github' : 'list',
-  testDir: './e2e', 
+  testDir: './e2e',
   use: {
     trace: 'on-first-retry',
+    baseURL: process.env.CI ? process.env.BASE_URL : 'http://localhost:3000',
   },
   projects: [
     {

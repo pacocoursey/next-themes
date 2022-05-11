d2c5f3627d71d624a9236e9cfe77ece9bdf85d0d
Tobias Maier <trm217@protonmail.com>
2022-05-11 22:40:33 +0200

    add forced-theme test-cases

--- e2e/forced-theme.test.ts
+++ e2e/forced-theme.test.ts
@@ -1,7 +1,23 @@
 import { test} from '@playwright/test'
+import { checkAppliedTheme, checkStoredTheme, makeBrowserContext } from './util'
 
 test.describe('forced theme test-suite', async () => {
-  test('should render forced-theme instead of saved theme', async () => {
-    // TODO
-  })
+  function makeForcedThemeTest(pageUrl: string, storedTheme: string, expectedTheme: string) {
+    test(
+      `should render forced-theme (${expectedTheme}) instead of stored theme (${expectedTheme})`, 
+      async ({ browser, baseURL}) => {
+        const context = await makeBrowserContext(browser, {
+          baseURL,
+          localStorage: [{ name: 'theme', value: storedTheme }]
+        })
+        const page = await context.newPage()
+        await page.goto(pageUrl)
+
+        await checkStoredTheme(page, storedTheme)
+        await checkAppliedTheme(page, expectedTheme)
+    })
+  }
+
+  makeForcedThemeTest('/light', 'dark', 'light')
+  makeForcedThemeTest('/dark', 'light', 'dark')
 })

5796b3897a67014e67d87509f962d9904cd948fb
Tobias Maier <trm217@protonmail.com>
2022-05-11 07:54:38 +0200

    add utility to create new browserContext for Playwright test

--- e2e/util.ts
+++ e2e/util.ts
@@ -1,4 +1,4 @@
-import { Page, expect } from "@playwright/test";
+import { Page, expect, Browser } from '@playwright/test'
 
 export async function checkAppliedTheme(page: Page,theme: string) {
     expect(
@@ -7,4 +7,25 @@ export async function checkAppliedTheme(page: Page,theme: string) {
     expect(
         await page.evaluate(() => document.documentElement.getAttribute('style'))
     ).toBe(`color-scheme: ${theme};`);
+}
+
+type MakeBrowserContextOptions = {
+    baseURL?: string,
+    colorScheme?: 'light' | 'dark' | 'no-preference',
+    localStorage?: {name: string, value: string}[]
+}
+
+export async function makeBrowserContext(browser: Browser, options: MakeBrowserContextOptions) {
+    return await browser.newContext({
+        colorScheme: options.colorScheme ?? 'no-preference',
+        storageState: {
+            cookies: [],
+            origins: [
+                {
+                    origin: options.baseURL ?? 'http://localhost:3000',
+                    localStorage: options.localStorage ?? [],
+                }
+            ]
+        }
+    })
 }

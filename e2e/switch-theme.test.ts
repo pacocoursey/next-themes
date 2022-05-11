2e545a1821fce72ac405099b73d78babf645590a
Tobias Maier <trm217@protonmail.com>
2022-05-11 22:42:59 +0200

    add e2e-test for basic theming use-cases

--- e2e/switch-theme.test.ts
+++ e2e/switch-theme.test.ts
@@ -1,21 +1,44 @@
 import { test} from '@playwright/test';
-import { checkAppliedTheme } from './util';
+import { checkAppliedTheme, makeBrowserContext } from './util';
 
-test.describe('theme switch test-suite', () => {
+test.describe('basic theming test-suite', () => {
 
-    test('should switch to dark-theme', async ({ page }) => {
-        await page.goto('/');
-        // Select dark
-        await page.locator('[data-test-id="theme-selector"]').selectOption('dark');
-        // Check if dark theme is applied
-        await checkAppliedTheme(page, 'dark');
-    });
+    function makeRenderThemeTest(theme: string) {
+        test(`should render ${theme} theme`, async ({ browser, baseURL }) => {
+            const context = await  makeBrowserContext(browser, { 
+                baseURL, 
+                localStorage: [{ name: 'theme', value: theme }] 
+            });
+            const page = await context.newPage()
 
-    test('should switch to light-theme', async ({ page }) => {
-        await page.goto('/');
-        // Select dark
-        await page.locator('[data-test-id="theme-selector"]').selectOption('light');
-        // Check if dark theme is applied
-        await checkAppliedTheme(page, 'light');
-    });
+            await page.goto('/');
+            // Select dark
+            await page.locator('[data-test-id="theme-selector"]').selectOption(theme);
+            // Check if dark theme is applied
+            await checkAppliedTheme(page, theme);
+        });
+    }
+
+    makeRenderThemeTest('light');
+    makeRenderThemeTest('dark');
+
+    function shouldUpdateTheme(initialTheme, targetTheme: string) {
+        test(`should switch from ${initialTheme} to ${targetTheme}-theme`, async ({ browser, baseURL }) => {
+                const context = await  makeBrowserContext(browser, { 
+                    baseURL, 
+                    localStorage: [{ name: 'theme', value: initialTheme }] 
+                });
+                const page = await context.newPage()
+
+                await page.goto('/');
+                await checkAppliedTheme(page, initialTheme);
+                // Select dark
+                await page.locator('[data-test-id="theme-selector"]').selectOption(targetTheme);
+                // Check if dark theme is applied
+                await checkAppliedTheme(page, targetTheme);
+            });
+        }
+
+    shouldUpdateTheme('light', 'dark');
+    shouldUpdateTheme('dark', 'light');
 })

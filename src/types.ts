428f7a1f8d9f1ba5d3f2a02a57b892df88ac6165
Bruno Crosier <bruno.crosier@gmail.com>
2022-07-27 16:06:22 +0000

    improve types

--- src/types.ts
+++ src/types.ts
@@ -19,21 +19,21 @@ export interface UseThemeProps {
 
 export interface ThemeProviderProps {
   /** List of all available theme names */
-  themes?: string[]
+  themes: string[]
   /** Forced theme name for the current page */
   forcedTheme?: string
   /** Whether to switch between dark and light themes based on prefers-color-scheme */
-  enableSystem?: boolean
+  enableSystem: boolean
   /** Disable all CSS transitions when switching themes */
-  disableTransitionOnChange?: boolean
+  disableTransitionOnChange: boolean
   /** Whether to indicate to browsers which color scheme is used (dark or light) for built-in UI like inputs and buttons */
-  enableColorScheme?: boolean
+  enableColorScheme: boolean
   /** Key used to store theme setting in localStorage */
-  storageKey?: string
+  storageKey: string
   /** Default theme name (for v0.0.12 and lower the default was light). If `enableSystem` is false, the default theme is light */
-  defaultTheme?: string
+  defaultTheme: string
   /** HTML attribute modified based on the active theme. Accepts `class` and `data-*` (meaning any data attribute, `data-mode`, `data-color`, etc.) */
-  attribute?: string | 'class'
+  attribute: `data-${string}` | 'class'
   /** Mapping of theme name to HTML attribute value. Object where key is the theme name and value is the attribute value */
   value?: ValueObject
   /** Nonce string to pass to the inline script for CSP headers */

a2fa3c10f13bf98264b0f2ff5be6cd5db527f320
Tobias Maier <trm217@proton.me>
2023-07-24 17:14:01 +0200

    chore: convert intor a turbo based monorepo (yarn workspaces)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,28 +1,35 @@
-import { PlaywrightTestConfig, devices } from '@playwright/test';
+import { PlaywrightTestConfig, devices } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   reporter: process.env.CI ? 'github' : 'list',
   testDir: './e2e',
+  webServer: {
+    command: 'yarn run start --filter=with-tailwindcss...',
+    port: 3000,
+    reuseExistingServer: !process.env.CI,
+    stdout: 'ignore',
+    stderr: 'pipe'
+  },
   use: {
     trace: 'on-first-retry',
-    baseURL: process.env.CI ? process.env.BASE_URL : 'http://localhost:3000',
+    baseURL: process.env.CI ? process.env.BASE_URL : 'http://localhost:3000'
   },
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'] }
     },
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: { ...devices['Desktop Firefox'] }
     },
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-  ],
-};
+      use: { ...devices['Desktop Safari'] }
+    }
+  ]
+}
 
-export default config;
+export default config

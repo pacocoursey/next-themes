cad544856afc78941f568f5e8244d85597efaaa0
Tobias Maier <trm217@proton.me>
2023-08-28 09:28:11 +0200

    chore: adds a new example app called 'with-app-dir'

---
+++ examples/with-app-dir/tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  darkMode: 'class',
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {},
+  plugins: []
+}
+export default config

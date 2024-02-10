4d7fba80fe683b5c92104009fefdebf1321463df
BlankParticle <blankparticle@gmail.com>
2024-02-10 12:08:55 +0530

    fix: add 'use client' banner to output

--- packages/next-themes/tsup.config.js
+++ packages/next-themes/tsup.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   sourcemap: false,
   minify: true,
+  banner: {
+    js: `'use client'`
+  },
   dts: true,
   format: ['esm', 'cjs'],
   loader: {

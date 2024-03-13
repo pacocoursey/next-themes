fa4ed748388bfcbb0e4f9729b2fedc21c768f32a
paco <34928425+pacocoursey@users.noreply.github.com>
2024-03-13 12:40:43 -0400

    Update next-themes/src/index.tsx

--- next-themes/src/index.tsx
+++ next-themes/src/index.tsx
@@ -12,7 +12,7 @@ const defaultContext: UseThemeProps = { setTheme: _ => {}, themes: [] }
 
 export const useTheme = () => React.useContext(ThemeContext) ?? defaultContext
 
-export const ThemeProvider = (props: ThemeProviderProps):React.ReactNode => {
+export const ThemeProvider = (props: ThemeProviderProps): React.ReactNode => {
   const context = React.useContext(ThemeContext)
 
   // Ignore nested context providers, just passthrough children

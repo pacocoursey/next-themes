b14730bc54de51a2ddd96cf6a2f0a9b1563848fd
Mayank <mayank.srmu@gmail.com>
2024-06-13 15:29:07 +0530

    fix: remove unnecessary `| undefined` from types

--- next-themes/src/types.ts
+++ next-themes/src/types.ts
@@ -8,38 +8,38 @@ export interface UseThemeProps {
   /** List of all available theme names */
   themes: string[]
   /** Forced theme name for the current page */
-  forcedTheme?: string | undefined
+  forcedTheme?: string
   /** Update the theme */
   setTheme: React.Dispatch<React.SetStateAction<string>>
   /** Active theme name */
-  theme?: string | undefined
+  theme?: string
   /** If `enableSystem` is true and the active theme is "system", this returns whether the system preference resolved to "dark" or "light". Otherwise, identical to `theme` */
-  resolvedTheme?: string | undefined
+  resolvedTheme?: string
   /** If enableSystem is true, returns the System theme preference ("dark" or "light"), regardless what the active theme is */
-  systemTheme?: 'dark' | 'light' | undefined
+  systemTheme?: 'dark' | 'light'
 }
 
 export type Attribute = `data-${string}` | 'class'
 
 export interface ThemeProviderProps extends React.PropsWithChildren {
   /** List of all available theme names */
-  themes?: string[] | undefined
+  themes?: string[]
   /** Forced theme name for the current page */
-  forcedTheme?: string | undefined
+  forcedTheme?: string
   /** Whether to switch between dark and light themes based on prefers-color-scheme */
-  enableSystem?: boolean | undefined
+  enableSystem?: boolean
   /** Disable all CSS transitions when switching themes */
-  disableTransitionOnChange?: boolean | undefined
+  disableTransitionOnChange?: boolean
   /** Whether to indicate to browsers which color scheme is used (dark or light) for built-in UI like inputs and buttons */
-  enableColorScheme?: boolean | undefined
+  enableColorScheme?: boolean
   /** Key used to store theme setting in localStorage */
-  storageKey?: string | undefined
+  storageKey?: string
   /** Default theme name (for v0.0.12 and lower the default was light). If `enableSystem` is false, the default theme is light */
-  defaultTheme?: string | undefined
+  defaultTheme?: string
   /** HTML attribute modified based on the active theme. Accepts `class`, `data-*` (meaning any data attribute, `data-mode`, `data-color`, etc.), or an array which could include both */
-  attribute?: Attribute | Attribute[] | undefined
+  attribute?: Attribute | Attribute[]
   /** Mapping of theme name to HTML attribute value. Object where key is the theme name and value is the attribute value */
-  value?: ValueObject | undefined
+  value?: ValueObject
   /** Nonce string to pass to the inline script for CSP headers */
-  nonce?: string | undefined
+  nonce?: string
 }
